5e4ebfbbd9925d000b12b96e8a08f67e9ee9c5de
Lewis Evans <lewisevansuk@gmail.com>
2024-10-19 21:08:56 +0100

    add description variant

--- islands/Toast.tsx
+++ islands/Toast.tsx
@@ -2,14 +2,25 @@ import { toast, Toaster } from "sonner";
 
 export default function Toast() {
   return (
-    <div class="m-auto">
+    <>
       <Toaster />
-      <button
-        class="bg-neutral-950 text-neutral-100 hover:bg-neutral-800 rounded-md text-sm font-medium transition-colors h-10 px-4 py-2"
-        onClick={() => toast("My first toast")}
-      >
-        Give me a toast
-      </button>
-    </div>
+      <div class="max-w-screen-md mx-auto flex flex-row gap-2 py-4">
+        <button
+          class="bg-neutral-950 text-neutral-100 hover:bg-neutral-800 rounded-md text-sm font-medium transition-colors h-10 px-4 py-2"
+          onClick={() => toast("My first toast")}
+        >
+          Give me a toast
+        </button>
+        <button
+          class="bg-neutral-950 text-neutral-100 hover:bg-neutral-800 rounded-md text-sm font-medium transition-colors h-10 px-4 py-2"
+          onClick={() =>
+            toast.message("Event has been created", {
+              description: "Monday, January 3rd at 6:00pm",
+            })}
+        >
+          Description
+        </button>
+      </div>
+    </>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,25 +2,27 @@ import Toast from "../islands/Toast.tsx";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Fresh with Sonner</h1>
-        <p class="my-4">
-          Try out the different toasts below provided by{" "}
-          <a class="underline" href="https://sonner.emilkowal.ski/">
-            sonner
-          </a>
-          .
-        </p>
-        <Toast />
+    <>
+      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <img
+            class="my-6"
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the Fresh logo: a sliced lemon dripping with juice"
+          />
+          <h1 class="text-4xl font-bold">Fresh with Sonner</h1>
+          <p class="my-4">
+            Try out the different toasts below provided by{" "}
+            <a class="underline" href="https://sonner.emilkowal.ski/">
+              sonner
+            </a>
+            .
+          </p>
+        </div>
       </div>
-    </div>
+      <Toast />
+    </>
   );
 }
